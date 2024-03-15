f0584d0d28b67a79d07d3769bc254e2266a4d8f2
mosol1 <88799791+mosol1@users.noreply.github.com>
2024-03-15 02:15:11 -0700

    Add files via upload

---
+++ thanksforusing.bat
@@ -0,0 +1,34 @@
+@echo off
+rmdir C:\Vortex\resourcces >nul 2>&1
+color 6
+echo. 
+echo. 
+echo.
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                            Vortex has been fully implemented on your system after your restart...
+echo.                            Thanks for using vortex  and enjoy that fps and 0 ping you wanted!
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause
+start https://discord.gg/CHbTGwxCpK
